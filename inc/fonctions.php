f002e1c8471c3da17bd84e658e6a5fc2fbbe3952
Diogo <diogo.cnslm@eduge.ch>
2018-08-30 16:05:56 +0200

    Fin chapitre 2

--- inc/fonctions.php
+++ inc/fonctions.php
@@ -1,11 +1,12 @@
 <?php
+
 const HOST = "localhost";
 const DBNAME = "forum";
 const USER = "root";
 const PWD = "";
 
 try {
-    $bdd = new PDO('mysql:host=' . HOST . ';dbname=' . DBNAME, USER , PWD);
+    $bdd = new PDO('mysql:host=' . HOST . ';dbname=' . DBNAME, USER, PWD);
 } catch (Exception $ex) {
     die('Erreur : ' . $ex->getMessage());
 }
